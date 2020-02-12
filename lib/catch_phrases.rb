81c7293d15bb19b8cca7e73ed719cb063809e35c
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-12 21:04:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,12 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
   # status = "It's Dangerous To Go Alone! Take This."
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
